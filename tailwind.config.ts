69f013b13f133220d4d1ab41dec1df129370b5cd
Daniel Catania <dcataniarodrigues20@gmail.com>
2025-06-26 18:11:56 -0300

    feat: add default colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#94D82D",
+          150: "#83AE42",
+        },
+        secondary: {
+          100: "#9DA0F0F",
+          150: "#AD0808",
+        },
+        dark: "#262626",
+        light: "#F9F9F9",
+        neutral: "#9E9E9E",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
