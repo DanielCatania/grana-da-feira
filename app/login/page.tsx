93adff94234ff2a187a93221c110b483fa19dd2c
Daniel Catania <dcataniarodrigues20@gmail.com>
2025-06-27 15:15:49 -0300

    feat: add geometric background

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export default function Login() {
+  return (
+    <div className="bg-geometric">
+      <main className="overlay">
+        <h1>Login</h1>
+      </main>
+    </div>
+  );
+}
