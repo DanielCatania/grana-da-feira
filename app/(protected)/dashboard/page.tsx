79c84d27304775fb6b305ff4dd1da66f319c885e
Daniel Catania <dcataniarodrigues20@gmail.com>
2025-07-01 23:21:00 -0300

    feat: add option to change password in dashboard

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
 export default async function Dashboard() {
   return (
     <div className="w-full min-h-screen flex-col flex items-center justify-center">
       <main className="w-2/5 min-w-60">
         <div>
           <p className="text-2xl">Olá</p>
+        </div>
+        <div>
+          <Link href="/change-password" className="text-primary-100 underline">
+            Trocar senha
+          </Link>
         </div>
       </main>
     </div>
