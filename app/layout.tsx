7fa7a5e978b38848be865a34993ef6af562bfbf5
Daniel Catania <dcataniarodrigues20@gmail.com>
2025-08-07 17:48:41 -0300

    feat: add header with developer information to RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import defaultUrl from "@/utils/defaultUrl";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl()),
@@ -17,6 +18,19 @@ export default function RootLayout({
   return (
     <html lang="pt-br" suppressHydrationWarning>
       <body>
+        <header className="flex justify-center items-center backdrop-blur-md text-xs mb-2">
+          <p>
+            Desenvolvido por{" "}
+            <Link
+              href="https://github.com/DanielCatania"
+              target="_blank"
+              className="underline text-primary-100"
+            >
+              ©Daniel Catania
+            </Link>{" "}
+            - 2025
+          </p>
+        </header>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
