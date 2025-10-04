66edede345519d6a6c7ed1b906fb48169072da48
Daniel Catania <dcataniarodrigues20@gmail.com>
2025-10-04 15:53:53 -0300

    feat: enhance Records page with user access and purchase ID counts

--- app/(protected)/admin/records/page.tsx
+++ app/(protected)/admin/records/page.tsx
@@ -3,6 +3,7 @@ import { PostgrestError } from "@supabase/supabase-js";
 import { redirect } from "next/navigation";
 
 import Header from "@/components/Header";
+import Box from "@/components/Box";
 import BackButton from "@/components/Button/BackButton";
 import TransactionsList from "./components/TransactionsList";
 import { transactionContent } from "./components/TransactionsList/type";
@@ -10,15 +11,36 @@ import { transactionContent } from "./components/TransactionsList/type";
 export default async function Records() {
   const supabase = await createClient();
 
-  const { data: transactions, error: transactionError } = (await supabase.from(
-    "Transaction"
-  ).select(`
+  const { data: transactions, error: transactionError } = (await supabase
+    .from("Transaction")
+    .select(
+      `
     *,
     user:User(id, name)
-  `)) as { data: transactionContent[]; error: PostgrestError | null };
+  `
+    )
+    .order("date", { ascending: false })) as {
+    data: transactionContent[];
+    error: PostgrestError | null;
+  };
 
-  if (transactionError !== null) {
-    console.error("Error fetching trasactions data:", transactionError);
+  const { count: purchaseIdsCount, error: purchaseError } = await supabase
+    .from("PurchaseId")
+    .select("*", { count: "exact", head: true });
+
+  const { count: initialAccessUsersCount, error: usersCountError } =
+    await supabase
+      .from("User")
+      .select("*", { count: "exact", head: true })
+      .eq("passworddefault", false);
+
+  if (transactionError || usersCountError || purchaseError) {
+    console.error(
+      "Error fetching data:",
+      transactionError,
+      usersCountError,
+      purchaseError
+    );
     redirect("/admin");
   }
 
@@ -34,7 +56,26 @@ export default async function Records() {
       <Header>
         <BackButton href="/admin" />
       </Header>
+      <h1 className="text-3xl font-extrabold text-primary-150 mb-4">
+        Recordes da plataforma
+      </h1>
+      <Box title="Total de Usuários com Acesso Inicial">
+        <p className="text-primary-100 text-justify font-bold">
+          <span className="font-extrabold text-primary-150">
+            {initialAccessUsersCount} usuários
+          </span>{" "}
+          já acessaram o sistema com a senha padrão.
+        </p>
+      </Box>
       <TransactionsList transactions={donations} isDonation={true} />
+      <Box title="Total de IDs de Compra Gerados">
+        <p className="text-primary-100 text-justify font-bold">
+          <span className="font-extrabold text-primary-150">
+            {purchaseIdsCount} IDs de compra
+          </span>{" "}
+          foram gerados até agora.
+        </p>
+      </Box>
       <TransactionsList transactions={purchases} />
     </>
   );
